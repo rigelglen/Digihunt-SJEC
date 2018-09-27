b87afcceae410638e005d8db8f85052861a82534
Rigel <rigelglen@gmail.com>
2018-09-27 23:27:08 +0530

    Added gitlab CI/CD

--- src/consts.js
+++ src/consts.js
@@ -1,9 +1,12 @@
-export const codes = ["JT8DZdWCtfn0ZrI", "kd32fMHUGAYcLFe", "ovssUvoCP5RTAeY", "Jn2MNONSzaIsh6X", "9wK5L0tZZwEuyKe", "Eg3tt0G0QhlwmuE", "gaDkqegWLDrYd3d"];
+const codes = ["JT8DZdWCtfn0ZrI", "kd32fMHUGAYcLFe", "ovssUvoCP5RTAeY", "Jn2MNONSzaIsh6X", "9wK5L0tZZwEuyKe", "Eg3tt0G0QhlwmuE", "gaDkqegWLDrYd3d"];
 
 var fs = require("fs");
 
-export const saveList = (userState) => {
+const saveList = (userState) => {
   fs.writeFile("src/filename.json", JSON.stringify(userState), "utf8", () => {
     console.log("saved");
   });
 }
+
+exports.codes = codes;
+exports.saveList = saveList;
